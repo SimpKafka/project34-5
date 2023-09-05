a7be46b0dc74424484c41e4fa1daa3cd4683d2fe
Anothai-Pongpilaiplued <weaponkung02@gmail.com>
2023-09-05 10:45:00 +0700

    อัพเดท Authen

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\PromotionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,3 +38,10 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('/admin',[HomeController::class, 'admin']);
+
+//เมนูสินค้า
+Route::get('admin/product/index', [ProductController::class, 'index'])->name('p.index');
+
+
+//เมนูโปรโมชั่น
+Route::get('admin/promotion/index', [PromotionController::class, 'index'])->name('pro.index');
